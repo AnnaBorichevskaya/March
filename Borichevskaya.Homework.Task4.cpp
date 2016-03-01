d11884e40d96b876c121c80d9dde88868561ba2c
AnnaBorichevskaya <borichevskaya98@mail.ru>
2016-03-01 20:05:44 +0200

    Update Borichevskaya.Homework.Task4.cpp

--- Borichevskaya.Homework.Task4.cpp
+++ Borichevskaya.Homework.Task4.cpp
@@ -2,48 +2,39 @@
 #include <string.h>
 using namespace std;
 
-void ChangeString(char*, char*);
+void Delete(char*, char*);
 bool TheSame(char*string, char*word, int, int);
 
 int main()
 {
 	const int length = 256;
 	char string[length], words[length];
-	cout << "Enter the string: ";
+	cout << "Enter the text: ";
 	cin.getline(string, length, '\n');
 	cout << "Enter words through the delete: ";
 	cin.getline(words, length, '\n');
 	system("cls");
 	cout << "\n\t String :\n";
 	cout << string;
-	ChangeString(string, words);
+	Delete(string, words);
 	cout << "\n\t New String :\n";
 	cout << string;
 	system("pause");
 	return 0;
 }
 
-void ChangeString(char* string, char* words)
+void Delete(char* string, char* words)
 {
 	char* symbols = "ABCDEFGHIJKLMNOPQRSTUWVXYZabcdefghijklnmopqrstuwvxyz";
 	char* pWord = strpbrk(string, symbols);
-	char*  oneWord = strpbrk(words, symbols);
-
-	while (oneWord)
+	while (pWord)
 	{
-		int leng = strspn(oneWord, symbols);
-		pWord = strpbrk(string, symbols);
-		while (pWord)
-		{
-			int length = strspn(pWord, symbols);
-			if (TheSame(pWord, oneWord, length, leng))
-				strcpy(pWord, pWord + length);
-			else
-				pWord += length;
-			pWord = strpbrk(pWord, symbols);
-		}
-		oneWord += leng;
-		oneWord = strpbrk(oneWord, symbols);
+		int length = strspn(pWord, symbols);
+		if (TheSame(pWord, oneWord, length, leng))
+			strcpy(pWord, pWord + length);
+		else
+			pWord += strlen(word);
+		pWord = strpbrk(pWord, symbols);
 	}
 }
 
